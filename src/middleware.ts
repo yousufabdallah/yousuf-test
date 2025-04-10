59369c14df91f7f34d2b9f512ffcbea54bcc4e3f
Your Name <your-email@example.com>
2025-04-10 17:20:09 +0400

    yousuf-6

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([
+'/dashboard(.*)',
+'/',
+'/forum(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
@@ -10,3 +17,4 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 }
+
